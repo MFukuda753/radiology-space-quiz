2681bf3ef0af44c720dec61feebac1db754951ce
fooldog22 <fooldog22@gmail.com>
2026-09-15 16:58:57 +0900

    1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789458714|963155965';
+const CACHE_VERSION = '1789459113|21816507';
 /** @type {string} */
 const CACHE_PREFIX = 'Oral radiology q-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
